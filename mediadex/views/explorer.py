c8ae5006b2e999558d008fc2fbff14bc1d37c2e2
Emhisy <49534860+Emhisy@users.noreply.github.com>
2025-05-28 13:43:11 +0200

    add css in popular manga page, fix tailwind config

--- mediadex/views/explorer.py
+++ mediadex/views/explorer.py
@@ -5,10 +5,11 @@ from ..constants.sources_constant import SOURCES
 @login_required(login_url="/login/")
 def explorer_home(request):
     context = { "sources": SOURCES.keys() }
-    return render(request, 'explorer/index.html', context)
+    return render(request, 'explorer/select_source.html', context)
 
 @login_required(login_url="/login/")
 def explorer_source(request, source_name):
     source = SOURCES[source_name]()
     context = { "mangas": source.get_popular_manga() }
-    return render(request, 'explorer/sources/index.html', context)
+    print(context["mangas"][0].keys())
+    return render(request, 'explorer/sources/popular_manga.html', context)
--- project/settings.py
+++ project/settings.py
@@ -34,6 +34,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TAILWIND_APP_NAME = 'theme'
 
 # Application definition
 
@@ -133,6 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'theme' / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
